1c8261064c552c16af4da2ca3ca140aac051e5a1
Laurent Ducos <laurent.ducos@biblibre.com>
2015-09-08 19:38:49 +0200

    good state

--- check_functions.sh
+++ check_functions.sh
@@ -4,22 +4,25 @@ if [[ ! -d logs/ ]]
 then
 mkdir logs
 fi
-# Checks
+
+# Start checks
 function loadavg {
+status=ok
+warning=3
+critical=5
 load_avg_1=$(cat /proc/loadavg|awk '{print $1}')
 load_avg_5=$(cat /proc/loadavg|awk '{print $2}')
 load_avg_15=$(cat /proc/loadavg|awk '{print $3}')
-warning=3
-critical=5
-status=ok
-#if [[ $load_avg_1 -ge $warning ]] && [[ $load_avg_1 -lt $critical ]]
-#then
-#$status=warning
-#fi
-#if [[ $load_avg_1 -ge $critical ]]
-#then
-#$status=critical
-#fi
+value=$(echo $load_avg_1|sed s/[.]/,/)
+
+if (( $(echo "$value $warning" | awk '{print ($1 > $2)}') )) && (( $(echo "$value $critical" | awk '{print ($1 < $2)}') ))
+then $status=warning
+fi
+
+if (( $(echo "$value $critical" | awk '{print ($1 > $2)}') ))
+then $status=critical
+fi
+
 echo "$(date +%s);$(hostname);load_avg;$load_avg_1:$load_avg_5:$load_avg_15;load;$status" >> logs/$(hostname)_loadavg.dat
 }
 
@@ -29,6 +32,6 @@ warning=
 critical=
 t=$(cat /proc/meminfo|grep MemFree|awk '{print $2}')
 mem_free=$((t/1024))
-echo "$(date +%s);$(hostname);mem_free;$mem_available;mb;$status" >> logs/$(hostname)_memfree.dat
+echo "$(date +%s);$(hostname);mem_free;$mem_free;mb;$status" >> logs/$(hostname)_memfree.dat
 }
 
