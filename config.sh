f410b45affe96b3704e8d82b7a78dcc6fb1ec1ad
Laurent Ducos <laurent.ducos@biblibre.com>
2015-09-09 13:18:06 +0200

    reorganisation des fichiers

--- config.sh
+++ config.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
+
 # General configuration
 lang=fr
-# Set test to run
-check_loadavg
-check_memory
 
+# Set test to run
+source functions.sh
+loadavg
+memory
+
+# Output
+source generate_html.sh
+headers $lang > webdatas/index.html
+footer >> webdatas/index.html
